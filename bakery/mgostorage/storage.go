a57c10ef31b00bfcd3176b73052de30150205c60
Roger Peppe <rogpeppe@gmail.com>
2014-12-08 13:39:31 +0000

    bakery/mgostorage: fix import paths

--- bakery/mgostorage/storage.go
+++ bakery/mgostorage/storage.go
@@ -8,7 +8,7 @@ import (
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 
-	"github.com/go-macaroon-bakery/macaroon-bakery/bakery"
+	"gopkg.in/macaroon-bakery.v0/bakery"
 )
 
 // New returns an implementation of Storage
